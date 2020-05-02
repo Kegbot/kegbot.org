817308ada6d66a42608b3cd07534df8f58edcf04
mike wakerly <opensource@hoho.com>
2020-05-02 13:56:33 -0400

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,25 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import { Container, Row, Col } from "react-bootstrap";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <h1>NOT FOUND</h1>
+          <p>
+            Oops! That page doesn't exist.
+          </p>
+          <p className="text-muted">
+            Looking for help? Try the <em>Discussion Forum</em> link
+            at the top.
+          </p>
+        </Col>
+      </Row>
+    </Container>
   </Layout>
 );
 
