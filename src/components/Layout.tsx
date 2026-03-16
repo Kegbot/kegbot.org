f87c4eb22c7d0e7b7b674dc23a152458b18c2206
mike wakerly <opensource@hoho.com>
2026-03-15 19:16:08 -0700

    misc: scroll to top on page navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,17 @@
 import { Box } from "@mui/material";
-import { Outlet } from "react-router";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router";
 
 import Footer from "./Footer";
 import Header from "./Header";
 
 export default function Layout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       <Header />
