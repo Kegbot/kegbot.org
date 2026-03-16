00a3d6d165c89ef08a28c21ae5696f6b59937954
mike wakerly <opensource@hoho.com>
2026-03-15 19:17:38 -0700

    analytics: add plausible

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,10 +1,15 @@
 import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
+import { init as initPlausible } from "@plausible-analytics/tracker";
 import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 import App from "./App";
+
+if (import.meta.env.PROD) {
+  initPlausible({ domain: "kegbot.org" });
+}
 
 const cache = createCache({ key: "css" });
 
